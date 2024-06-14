61225469b4770a62cb022b263c69426f803a009d
Arttu Puttonen <arskaputtonen@gmail.com>
2024-06-14 15:47:14 +0300

    väli push

--- index.js
+++ index.js
@@ -1,108 +1,89 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const Contact = require('./models/person');
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.json());
+app.use(express.static('dist'));
 
-
-app.use(morgan('tiny'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-]
+app.use(morgan('tiny'));
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (request, response) => {
-  response.send(persons)
-})
+  Contact.find({}).then(persons => {
+    response.json(persons);
+  });
+});
 
 app.get('/info', (request, response) => {
-    const date = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
-                  <p>${date}</p>`
-    response.send(info)
-  }
-)
+  Contact.countDocuments({}).then(count => {
+    const date = new Date();
+    const info = `<p>Phonebook has info for ${count} people</p>
+                  <p>${date}</p>`;
+    response.send(info);
+  });
+});
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-  
+  Contact.findById(request.params.id).then(person => {
     if (person) {
-      response.json(person)
+      response.json(person);
     } else {
-      response.status(404).end()
+      response.status(404).end();
     }
-  }
-)
+  }).catch(error => {
+    response.status(400).send({ error: 'malformatted id' });
+  });
+});
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-  
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }
-  
-    if (persons.find(person => person.name === body.name)) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: Math.floor(Math.random() * 1000)
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-  }
-)
+  const body = request.body;
 
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    });
+  }
+
+  const person = new Contact({
+    name: body.name,
+    number: body.number
+  });
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  });
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+  const id = request.params.id;
 
-app.use
+  Contact.findByIdAndDelete(id)
+    .then(deletedPerson => {
+      if (deletedPerson) {
+        response.status(204).end();
+      } else {
+        response.status(404).send({ error: 'Person not found' });
+      }
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' });
+    });
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  }
+});
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
